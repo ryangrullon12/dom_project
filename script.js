56707d1e20b60c9f90b7ea5c17ab31380b195b22
ryangrullon12 <ryangrullon123@gmail.com>
2024-07-29 15:36:04 -0400

    added changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Add your code to this file
+let mainClass = document.getElementsByClassName("pokemon_images");
+mainClass.style.border = "20px solid white";
+
+let changebodycolor = function(color) {
+    document.body.style.backgroundColor = color;
+    console.log(backgroundColor = (0,0,255));
+}
+
+let img = mainClass
+img[0,1,2].style.border = "500px solid orange";
+//i dont understand
